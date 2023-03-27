0e1c74fe1ae03454c0abfe294f15d629c5ee8048
hesam <bhesam2000@yahoo.com>
2023-03-27 10:47:08 +0300

    Initiallize repo

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { api } from "~/utils/api";
+
+const Home: NextPage = () => {
+  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+    <head>
+      <title>
+      zarp
+      </title>
+
+
+    </head>
+
+    </>
+  );
+};
+
+export default Home;
